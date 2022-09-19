980d7a73e147da8c69c2ca5e6a961cd63722bb9b
Gilberto Garza <55161386+gilgarzadev@users.noreply.github.com>
2022-09-19 11:31:34 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-
 //Array de productos
-
 
 let container = document.getElementById("container");
 let productos = [
-    {id: 1, Nombre: "Tomate", Precio: 54},
-    /* {id: 2, Nombre: "Aguacate", Precio: 18},
+  { id: 1, Nombre: "Tomate", Precio: 54 },
+  /* {id: 2, Nombre: "Aguacate", Precio: 18},
     {id: 3, Nombre: "Lechuga", Precio: 41},
     {id: 4, Nombre: "Pepino", Precio: 74},
     {id: 5, Nombre: "Chile", Precio: 10}, */
 ];
 
-let Nombre = prompt("Ingrese el nombre del producto");
-let Precio = prompt("Ingrese el precio del producto");
+const registroProductos = () => {
+  container.innerHTML = "";
+  let Nombre = prompt("Ingrese el nombre del producto");
+  let Precio = prompt("Ingrese el precio del producto");
 
-let altaProducto = {
-    id: productos.length +1,
+  let altaProducto = {
+    id: productos.length + 1,
     Nombre: Nombre,
     Precio: Precio,
-};
-productos.push(altaProducto);
+  };
+  productos.push(altaProducto);
 
-
-
-for(const producto of productos){
+  for (const producto of productos) {
     let div = document.createElement("div");
     div.innerHTML = `
         <h5>ID: ${producto.id}</h5>
@@ -32,15 +30,15 @@ for(const producto of productos){
     `;
 
     container.append(div);
-}
+  }
 
-(function(){
-    var saludo = function(){
-        alert("Gracias");
+  (function () {
+    var saludo = function () {
+      alert("Gracias");
     };
 
     var boton = document.getElementById("boton");
     boton.addEventListener("click", saludo);
-}
-
-())
+  })();
+};
+boton2.addEventListener("click", registroProductos);
